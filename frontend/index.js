d2575d5aa323c88cb151705573702ddafd75a37a
Edward Zhou <eddyspaghette@gmail.com>
2024-03-27 18:09:36 -0700

    remove common folder for now - add it back if needed

--- frontend/index.js
+++ frontend/index.js
@@ -2,17 +2,25 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require("path");
 const authRoutes = require(path.join(__dirname, 'routes', 'auth'))
-const { connectFunctionsEmulator } = require("firebase/functions");
 const { functions } = require(path.join(__dirname, 'common', 'fire'))
+const { connectFunctionsEmulator } = require("firebase/functions");
 
-connectFunctionsEmulator(functions, "localhost", 5001);
 const app = express();
 const PORT = 8080;
+const FUNCTIONS_PORT = 5001;
 
+// disable this when deploying to production
+connectFunctionsEmulator(functions, "localhost", FUNCTIONS_PORT);
+
+// middleware
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.json())
+
+// views && view engine
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
+// routes
 app.use('/', authRoutes)
 
 app.listen(PORT, () => {
