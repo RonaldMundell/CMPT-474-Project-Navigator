e9b8087391ec9b9727b8e8798b38babd88a754ba
Edward Zhou <eddyspaghette@gmail.com>
2024-04-06 00:26:56 -0700

    changed teacher service

--- services/teacher/index.js
+++ services/teacher/index.js
@@ -29,20 +29,59 @@ exports.getClassrooms = functions.https.onCall((data, context) => {
       'while authenticated');
   }
 
-  return db.collection('classroom').get().then((snapshot) => {
+  return db.collection('classroom').where("uid", "==", data.user).get().then((snapshot) => {
     const classrooms = [];
     snapshot.forEach((doc) => {
       classrooms.push(doc.data());
     });
-    console.log(classrooms)
     return classrooms;
   });
-
 });
+
+exports.addAssignment = functions.https.onCall((data, context) => {
+  if (data.length === 0) {
+    throw new functions.https.HttpsError("Invalid argument", 'the function must be called with ' +
+      'one arguments "text" containing the message text to add')
+  }
+  if (!context.auth) {
+    throw new functions.https.HttpsError('failed pre-condition', 'the function must be called' +
+      'while authenticated');
+  }
+  return addAssignmentToDb(data);
+});
+
+exports.getAssignments = functions.https.onCall((data, context) => {
+  if (data.length === 0) {
+    throw new functions.https.HttpsError("Invalid argument", 'the function must be called with ' +
+      'one arguments "text" containing the message text to add')
+  }
+  if (!context.auth) {
+    throw new functions.https.HttpsError('failed pre-condition', 'the function must be called' +
+      'while authenticated');
+  }
+  return db.collection('classroom').doc(data.classCode.toString()).collection('assignments').get().then((snapshot) => {
+    const assignments = [];
+    snapshot.forEach((doc) => {
+      assignments.push(doc.data());
+    });
+    return assignments;
+  });
+});
+
+function addAssignmentToDb(data) {
+  const { assignment, dueDate, index, classCode } = data;
+  return db.collection('classroom').doc(classCode.toString()).collection('assignments').doc(assignment.toString()).set({
+    assignment: assignment,
+    dueDate: dueDate,
+    classCode: classCode,
+    index: index
+  })
+}
 
 function addUserToClassDb(classCode, teacherName, uid) {
   return db.collection('classroom').doc(classCode.toString()).set({
     teacherName: teacherName,
+    classCode: classCode,
     uid: uid
   })
 }
